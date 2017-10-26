8b244bee201a1d0f4840e2a5cc4324e7895dba98
thomasyimgit <thomasyim94@gmail.com>
2017-10-26 17:31:54 -0400

    add async support

--- index.js
+++ index.js
@@ -6,4 +6,22 @@ function memoize(fn) {
   }
 }
 
-module.exports = memoize
+/**
+ * memoizeAsync ONLY accepts the function that returns a thenable object, for instance, a promise
+ */
+
+function memoizeAsync(fn) {
+  if (!fn.then) {
+    throw new Error('the function passed into memoizeAsync should return a thenable object, for instance, a promise')
+  }
+  let cache = Object.create(null)
+  return function(...args) {
+    if (JSON.stringify(args) in cache) return cache[JSON.stringify(args)]
+    return fn.apply(fn, args).then(res => cache[JSON.stringify(args)] = res)
+  }
+}
+
+module.exports = {
+  memoize,
+  memoizeAsync
+}
